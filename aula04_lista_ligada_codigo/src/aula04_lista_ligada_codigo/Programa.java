3637e23fbc8f8fd1c95efe5bd5b74a718492d705
saulocapistrano <saulocapistrano@gmail.com>
2023-05-12 14:36:01 -0300

    primeiro estudo sobre lista ligada com tipos

---
+++ aula04_lista_ligada_codigo/src/aula04_lista_ligada_codigo/Programa.java
@@ -0,0 +1,39 @@
+package aula04_lista_ligada_codigo;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		ListaLigada lista = new ListaLigada();
+		lista.adicionar("AC");
+		lista.adicionar("SP");
+		lista.adicionar("CE");
+		lista.adicionar("DF");
+		lista.adicionar("SP");
+		System.out.println();
+		System.out.println("A lista de Estados tem tamanho: "+lista.getTamanho());
+		System.out.println("Primeiro: "+lista.getPrimeiro().getValor());
+		System.out.println("Ultimo: "+lista.getUltimo().getValor());
+		System.out.println(lista.get(0).getValor());
+		System.out.println(lista.get(1).getValor());
+		System.out.println(lista.get(2).getValor());
+		System.out.println(lista.get(3).getValor());
+		
+		
+		for (int i= 0; i< lista.getTamanho(); i++) {
+			System.out.println(lista.get(i).getValor());
+		}
+		// Remover estado CE
+		
+		lista.remover("CE");
+		lista.remover("AC");
+		lista.remover("DF");
+		lista.remover("SP");
+		lista.remover("SP");
+		System.out.println("Removeu estado CE");
+		for (int i= 0; i< lista.getTamanho(); i++) {
+			System.out.println(lista.get(i).getValor());
+		}
+	}
+
+}
---
+++ aula04_lista_ligada_codigo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+module aula04_lista_ligada_codigo {
+}
